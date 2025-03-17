2fd5009483edf96bd1e951cdc7a951fbc60ea147
Gregory Futa <gregory.futa@sealights.io>
2025-03-17 13:45:22 +0100

    SL: build-005

--- scripts/line-coverage.sh
+++ scripts/line-coverage.sh
@@ -3,5 +3,5 @@
 java -jar scripts/sl-line-tool-1.0.30.jar \
 --workspace . \
 --tokenFile slTokenJava14.txt \
---buildSessionId 2d707b6a-51c6-43e4-9511-b620045b8119 \
+--buildSessionId 0520a956-ca94-4252-be74-6ee5e44b1277 \
 --referenceBuildSessionId d05235ec-1a92-4bd2-bd99-ae940941d15f
